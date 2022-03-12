4f9de40e0f490940fdf2e2fcf3a268f799797c33
kisha-net <antoinetteakinyi7@gmail.com>
2022-03-12 08:24:46 -0800

    Learning low level programming with C Language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Using printf
+ * Return: 0
+ */
 int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
