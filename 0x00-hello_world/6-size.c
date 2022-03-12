3aab1131b8b2369091c05eb033827bf3deccee2b
kisha-net <antoinetteakinyi7@gmail.com>
2022-03-12 08:28:40 -0800

    Learning low level programming with C Language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-#include <stdio.h
+#include <stdio.h>
 /**
  * main - Prints sizes of characters
  *
  * Return: Always 0 (success)
- */>
+ */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
